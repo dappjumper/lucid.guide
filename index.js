90fa3b686ed1f1cdd9064dfd3e340bcf6177a17f
Tobias Andersen <dappjump@gmail.com>
2020-04-13 15:12:46 +0200

    Boilerplated up!

---
+++ index.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var express  = require('express');
+// static file compression middleware
+var compress = require('compression');
+// middleware that allows you to parse request body, json, etc.
+var bodyParser = require('body-parser');
+
+// middleware to serve a favicon prior to all other assets/routes
+var favicon = require('serve-favicon');
+
+var app = express();
+
+app.use(bodyParser());
+
+app.use(compress());
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000);
+
+console.log('server started on port: ', process.env.PORT || 3000);
