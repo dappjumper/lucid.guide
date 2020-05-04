b3b15e886e69f62c90941d10ca4db35d50c94a3d
Tobias Andersen <dappjump@gmail.com>
2020-05-04 14:33:59 +0200

    Fix loader

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 var cors = require('cors');
 var bodyParser = require('body-parser')
-const version = "0.1.0"
+const version = "0.1.1"
 const port = process.env.PORT || 3000
 
 app.use(express.static('app/dist'));
